0048520b8b5fb26552a942d312e6cd7dca5af00e
Tatsuki Sugiura <sugi@nemui.org>
2019-04-15 20:41:47 +0900

    Stop to create alias.

--- lib/ya_kansuji/core_refine.rb
+++ lib/ya_kansuji/core_refine.rb
@@ -6,12 +6,11 @@ require 'ya_kansuji'
 module YaKansuji
   module CoreRefine
     refine String do
-      alias_method :_to_i_ya_kansuji_orig, :to_i unless defined?(_to_i_ya_kansuji_orig)
       def to_i(base = nil)
         if base.nil? || base == :kan || base == :kansuji
           YaKansuji.to_i self
         else
-          _to_i_ya_kansuji_orig base
+          super base
         end
       end
     end
